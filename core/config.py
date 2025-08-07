9b6d23873a9cef66524a933ea5e934b20ce9dbe2
Mã Hải Nhật <19521953@gm.uit.edu.vn>
2025-08-07 15:09:02 +0700

    upload config + toggle change color

---
+++ core/config.py
@@ -0,0 +1,23 @@
+# XÓA ĐI example. ở phía trước và điền key vào
+
+GEMINI_API_KEY: list[str] = ["XXXXX"]
+
+TOGETHERAI_API_KEY: str = ""
+
+GEMINI_ENDPOINTS = [
+    {
+        "name": "Lorem",
+        "endpoint": "",
+        "type": "gemini-xxx"
+    }
+]
+
+LLAMA_ENDPOINTS = [
+    {
+        "name": "Lorem",
+        "endpoint": "",
+        "type": "together-xxx"
+    }
+]
+
+ALL_ENDPOINTS = GEMINI_ENDPOINTS + LLAMA_ENDPOINTS
