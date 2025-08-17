d642a6c41e2d640c7244db587b3a2d8a9708af8a
ngwgsang <nguyenquangsang0709@gmail.com>
2025-08-17 20:58:43 +0700

    up

---
+++ core/endpoints.py
@@ -0,0 +1,24 @@
+
+
+ENDPOINTS = [
+    {
+        "name": "Gemini-2.0-Lite",
+        "endpoint": "gemini-2.0-flash-lite",
+        "cfg_mode": "agent",
+        "endpoint_provider": "google"
+    },
+    {
+        "name": "Gemini-2.0",
+        "endpoint": "gemini-2.0-flash",
+        "cfg_mode": "agent",
+        "endpoint_provider": "google"
+    },
+    {
+        "name": "Gemini-2.5",
+        "endpoint": "gemini-2.5-flash",
+        "cfg_mode": "agent",
+        "endpoint_provider": "google"
+    },
+]
+
+
